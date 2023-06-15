62d1694faecbcbcd8ce36575724f39804522ea00
Artem Motozov <amotozov@ugcs.com>
2023-06-15 14:01:17 +0200

    [-] MSDK5 OnBoard Example - tried to use OnboardKey for sending data. Receiving same error: E/DjiOnBoardMessageSender: Message 'Hello from MSDK5' sending failed: ErrorImp{errorType='CORE', errorCode='REQUEST_HANDLER_NOT_FOUND', innerCode='', description='null', hint='error code = -1'}

--- msdk5onboardpcexample/app/src/main/java/com/ugcs/dji/msdk5/example/DjiOnBoardMessageSender.kt
+++ msdk5onboardpcexample/app/src/main/java/com/ugcs/dji/msdk5/example/DjiOnBoardMessageSender.kt
@@ -1,9 +1,13 @@
 package com.ugcs.dji.msdk5.example
 
-import dji.sdk.keyvalue.value.payload.WidgetType
-import dji.sdk.keyvalue.value.payload.WidgetValue
+import dji.sdk.keyvalue.key.DJIKey
+import dji.sdk.keyvalue.key.KeyTools
+import dji.sdk.keyvalue.key.OnboardKey
+import dji.sdk.keyvalue.value.common.ComponentIndexType
+import dji.sdk.keyvalue.value.common.EmptyMsg
 import dji.v5.common.callback.CommonCallbacks.CompletionCallback
 import dji.v5.common.error.IDJIError
+import dji.v5.et.set
 import dji.v5.manager.aircraft.payload.PayloadCenter
 import dji.v5.manager.aircraft.payload.PayloadIndexType
 import dji.v5.manager.interfaces.IPayloadManager
@@ -21,8 +25,18 @@ class DjiOnBoardMessageSender(
 	val receivedDataFlow = MutableSharedFlow<String>()
 	val connectedPayloadConnection = mutableListOf<IPayloadManager>()
 	val handlers = mutableListOf<(data: ByteArray) -> Unit>()
+	var onboardSdkSendDataKey: DJIKey.ActionKey<ByteArray, EmptyMsg>
 
 	init {
+		try {
+			onboardSdkSendDataKey = KeyTools.createKey(
+				OnboardKey.KeySendDataToOnBoardSDKDevice,
+				ComponentIndexType.UP_TYPE_C
+			)
+		} catch (e: Exception) {
+			Timber.e("Error while creating key", e)
+			throw IllegalStateException("Error while creating key", e)
+		}
 		val payloadManager = PayloadCenter.getInstance().payloadManager[PayloadIndexType.EXTERNAL]!!
 		try {
 			payloadManager.init()
@@ -43,47 +57,26 @@ class DjiOnBoardMessageSender(
 			}
 		}
 		handlers.add(handler)
-		payloadManager.setWidgetValue(
-			WidgetValue(WidgetType.INPUT, 0, 0),
-			object : CompletionCallback {
-				override fun onSuccess() {
-					Timber.i("Widget callback on success")
-				}
-
-				override fun onFailure(error: IDJIError) {
-					Timber.i("Widget callback on error", error)
-				}
-
-			})
 		payloadManager.addPayloadDataListener(handler)
-		payloadManager.addPayloadWidgetInfoListener {
-			Timber.i("[%s] Widget info: %s", PayloadIndexType.EXTERNAL, it)
-		}
-		payloadManager.addPayloadBasicInfoListener {
-			Timber.i("[%s] Payload basic info listener: %s", PayloadIndexType.EXTERNAL, it)
-		}
-
 	}
 
 	fun send() {
-		val payloadManager = PayloadCenter.getInstance().payloadManager[PayloadIndexType.EXTERNAL]!!
 		val message = "Hello from MSDK5"
 		val bytes = message.toByteArray()
 		Timber.i(
 			"Sending message '%s'[%d bytes] to the payload [%s].",
 			message, bytes.size, PayloadIndexType.EXTERNAL
 		)
-		payloadManager.sendDataToPayload(bytes, object : CompletionCallback {
-			override fun onSuccess() {
+		onboardSdkSendDataKey.set(bytes,
+			onSuccess = {
 				Timber.i("Message '%s' sent successfully.", message)
 				coroutineScope.launch {
 					receivedDataFlow.emit("Send message: '${message}'")
 				}
-			}
-
-			override fun onFailure(error: IDJIError) {
+			},
+			onFailure = { error ->
 				Timber.e("Message '%s' sending failed: %s", message, error)
 			}
-		})
+		)
 	}
 }
